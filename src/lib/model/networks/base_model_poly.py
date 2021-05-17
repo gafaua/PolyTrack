0798d8c2e238c805deb4a45e314929db009a269e
Gaspar Faure <gasparfaure@hotmail.com>
2021-05-17 15:51:07 -0400

    Adapting base_model to CenterPoly, also simplifying the heads generating code

--- src/lib/model/networks/base_model_poly.py
+++ src/lib/model/networks/base_model_poly.py
@@ -4,12 +4,24 @@ from __future__ import print_function
 
 import torch
 from torch import nn
+from lib.model.networks.base_modules import ConvBlock
 
 def fill_fc_weights(layers):
     for m in layers.modules():
         if isinstance(m, nn.Conv2d):
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
+
+def make_head(head_kernel, last_channel, convs, out_channels):
+    sequence = [ConvBlock(head_kernel, last_channel, convs[0], with_bn=False)]
+
+    for i in range(1, len(convs)):
+        sequence.append(nn.Conv2d(convs[i-1], convs[i], (1,1)))
+        sequence.append(nn.ReLU(inplace=True))
+
+    sequence.append(nn.Conv2d(convs[-1], out_channels, kernel_size=(1,1)))
+
+    return nn.Sequential(*sequence)
 
 class BaseModelPoly(nn.Module):
     def __init__(self, heads, head_convs, num_stacks, last_channel, opt=None):
@@ -21,48 +33,24 @@ class BaseModelPoly(nn.Module):
           head_kernel = 3
         self.num_stacks = num_stacks
         self.heads = heads
+
+        # TODO:Check possible compatibility issues when using head_convs in the heads generation
+        # For now, for the same behavior, from CenterPoly code head_convs must be [dims[0]]
         for head in self.heads:
-            classes = self.heads[head]
-            head_conv = head_convs[head]
-            if len(head_conv) > 0:
-              out = nn.Conv2d(head_conv[-1], classes, 
-                    kernel_size=1, stride=1, padding=0, bias=True)
-              conv = nn.Conv2d(last_channel, head_conv[0],
-                               kernel_size=head_kernel, 
-                               padding=head_kernel // 2, bias=True)
-              convs = [conv]
-              for k in range(1, len(head_conv)):
-                  convs.append(nn.Conv2d(head_conv[k - 1], head_conv[k], 
-                               kernel_size=1, bias=True))
-              if len(convs) == 1:
-                fc = nn.Sequential(conv, nn.ReLU(inplace=True), out)
-              elif len(convs) == 2:
-                fc = nn.Sequential(
-                  convs[0], nn.ReLU(inplace=True), 
-                  convs[1], nn.ReLU(inplace=True), out)
-              elif len(convs) == 3:
-                fc = nn.Sequential(
-                    convs[0], nn.ReLU(inplace=True), 
-                    convs[1], nn.ReLU(inplace=True), 
-                    convs[2], nn.ReLU(inplace=True), out)
-              elif len(convs) == 4:
-                fc = nn.Sequential(
-                    convs[0], nn.ReLU(inplace=True), 
-                    convs[1], nn.ReLU(inplace=True), 
-                    convs[2], nn.ReLU(inplace=True), 
-                    convs[3], nn.ReLU(inplace=True), out)
-              if 'hm' in head:
-                fc[-1].bias.data.fill_(opt.prior_bias)
-              else:
-                fill_fc_weights(fc)
-            else:
-              fc = nn.Conv2d(last_channel, classes, 
-                  kernel_size=1, stride=1, padding=0, bias=True)
-              if 'hm' in head:
-                fc.bias.data.fill_(opt.prior_bias)
-              else:
-                fill_fc_weights(fc)
-            self.__setattr__(head, fc)
+            out_channels = self.heads[head]
+            convs = head_convs[head]
+
+            module = nn.ModuleList([
+                make_head(head_kernel, last_channel, convs, out_channels)
+                for _ in self.num_stacks
+            ])
+
+            self.__setattr__(head, module)
+
+            if 'hm' in head:
+                for heat in self.__getattr__(head):
+                    heat[-1].bias.data.fill_(-2.19)
+
 
     def img2feats(self, x):
       raise NotImplementedError
