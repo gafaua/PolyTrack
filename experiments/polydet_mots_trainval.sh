bafa7979e90b3fbfe44808b1e178f38e034a5887
Gaspar <gasparfaure@hotmail.com>
2021-07-05 09:43:07 -0400

    Testing on hourglass backbone

--- experiments/polydet_mots_trainval.sh
+++ experiments/polydet_mots_trainval.sh
@@ -2,20 +2,23 @@ cd src
 
 # Training
 
-python main.py tracking,polydet \
---dataset mots --exp_id mots_trainval_dla34 --dataset_version train_val \
---val_intervals 2 --num_epochs 100 \
---hm_disturb 0.05 --lost_disturb 0.5 --fp_disturb 0.1 \
---pre_hm --same_aug --elliptical_gt --nbr_points 32 \
---lr_step 45,70 \
---batch_size 8 \
+# python main.py tracking,polydet \
+# --dataset mots --exp_id mots_trainval_hg_pre --dataset_version train_val \
+# --val_intervals 1 --num_epochs 150 \
+# --hm_disturb 0.05 --lost_disturb 0.35 --fp_disturb 0.1 \
+# --pre_hm --same_aug --elliptical_gt --nbr_points 32 \
+# --arch hourglass \
+# --lr_step 45,70 \
+# --batch_size 2 \
+# --load_model ../models/ctdet_coco_hg.pth
 
 # Testing
 
 python test.py tracking,polydet \
---dataset mots --exp_id mots_trainval_dla34 --dataset_version train_val \
---test_dataset mots \
+--dataset mots --exp_id mots_trainval_hg_pre --dataset_version train_val \
+--test_dataset mots --arch hourglass \
 --pre_hm --same_aug --elliptical_gt --nbr_points 32 \
---load_model ../exp/tracking,polydet/mots_trainval_dla34/model_best.pth \
+--load_model ../exp/tracking,polydet/mots_trainval_hg_pre/model_best.pth \
+--track_thresh 0.4 --max_age 32
 
 cd ..
--- experiments/testing_polydet.sh
+++ experiments/testing_polydet.sh
@@ -1,20 +1,22 @@
 cd src
 # debugging
 
-python testing.py tracking,polydet --exp_id mots_trainval_dla34 --dataset_version train_val \
---dataset mots --test_dataset mots \
---pre_hm --same_aug --elliptical_gt --debug 2 \
---nbr_points 32  \
---load_model ../exp/tracking,polydet/mots_trainval_dla34/model_best.pth
+# python testing.py tracking,polydet --exp_id mots_trainval_hg --dataset_version train_val \
+# --dataset mots --test_dataset mots \
+# --pre_hm --same_aug --elliptical_gt --debug 2 \
+# --nbr_points 32  --arch hourglass \
+# --load_model ../exp/tracking,polydet/mots_trainval_hg/model_best.pth
 
 #python test.py tracking,polydet --exp_id mots_testing --dataset_version train_val --test_dataset mots --pre_hm --same_aug --elliptical_gt --poly_weight 1 --nbr_points 32 --load_model ../exp/tracking,polydet/mots_testing/model_last.pth
 
 # testing demo on validation set
 
-# python demo.py tracking,polydet --exp_id mots_trainval_dla34 \
-# --dataset_version train_val --test_dataset mots \
-# --pre_hm --same_aug --elliptical_gt --nbr_points 32 \
-# --load_model ../exp/tracking,polydet/mots_trainval_dla34/model_best.pth \
-# --demo ../data/MOTS/train/MOTS20-09/img1/ --save_video \
+python demo.py tracking,polydet --exp_id mots_trainval_hg_pre \
+--dataset_version train_val --test_dataset mots \
+--pre_hm --same_aug --elliptical_gt --nbr_points 32 \
+--load_model ../exp/tracking,polydet/mots_trainval_hg_pre/model_best_67.pth \
+--demo ../data/MOTS/test/MOTS20-06/img1/ --save_video \
+--track_thresh 0.5 --max_age 32 --arch hourglass
+
 
 cd ..
